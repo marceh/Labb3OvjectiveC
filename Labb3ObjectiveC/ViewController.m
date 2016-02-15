6c155a42c65a5594dd8997197ad3e32fc3b4a2e7
Marcus <iths@Apples-MacBook-Pro-7.local>
2016-02-15 08:45:33 +0100

    gave up

--- Labb3ObjectiveC/ViewController.m
+++ Labb3ObjectiveC/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic) NSMutableArray *items;
+
 @end
 
 @implementation ViewController
@@ -17,11 +19,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.items = @[@{@"name" : @"Take out the trash", @"category" : @"Home"}, @{@"name" : @"Go shopping", @"category" : @"Home"}].mutableCopy;
+    self.navigationItem.title = @"To-do list";
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewItem:)];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-addItem
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.items.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"cellInTableView"  forIndexPath: indexPath];
+    NSDictionary *item = self.items[indexPath.row];
+    cell.textLabel.text = item[@"name"];
+    return cell;
+    
+}
+
 
 @end
